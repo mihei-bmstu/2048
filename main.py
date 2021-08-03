1f392903726e9606d979029cdf5f6b6c5494a1e3
mihei-bmstu <mihei-bmstu@mail.ru>
2021-08-03 18:00:03 +0300

    commit after second lesson. Created tests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 def pretty_print(arr):
@@ -10,8 +11,22 @@ def pretty_print(arr):
 
 
 def convert_ij_to_number(size, i, j):
-    """"converts given cell coordinates i and j to a cell number(starting from 1)"""
+    """"converts given cell index i and j to a cell number(starting from 1)"""
     return i * size + j + 1
+
+
+def get_index_from_number(size, num):
+    """"converts given cell number to index """
+    num -= 1
+    return num // size, num % size
+
+
+def insert_2_or_4(arr, x, y):
+    if random.random() <= 0.75:
+        arr[x][y] = 2
+    else:
+        arr[x][y] = 4
+    return arr
 
 
 def get_empty_cells(arr):
---
+++ tests.py
@@ -0,0 +1,53 @@
+import unittest
+from main import *
+
+
+class Test2048(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(convert_ij_to_number(4, 1, 1), 6)
+
+    def test_2(self):
+        self.assertEqual(convert_ij_to_number(4, 3, 3), 16)
+
+    def test_3(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        arr = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_cells(arr), a)
+
+    def test_4(self):
+        a = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        arr = [
+            [2, 2, 2, 4],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_cells(arr), a)
+
+    def test_5(self):
+        a = []
+        arr = [
+            [2, 2, 2, 4],
+            [2, 2, 2, 4],
+            [2, 2, 2, 4],
+            [2, 2, 2, 4],
+        ]
+        self.assertEqual(get_empty_cells(arr), a)
+
+    def test_6(self):
+        self.assertEqual(get_index_from_number(4, 1), (0, 0))
+
+    def test_7(self):
+        self.assertEqual(get_index_from_number(4, 16), (3, 3))
+
+    def test_8(self):
+        self.assertEqual(get_index_from_number(4, 6), (1, 1))
+
+if __name__ == "main":
+    unittest.main()
