5f34a9d86c28f0a9dd0b2de260ddf97bc24b6fb5
mihei-bmstu <mihei-bmstu@mail.ru>
2021-08-05 19:42:58 +0300

    commit lesson 11. DB created, initial screen created

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+bd = sqlite3.connect("2048.sqlite")
+
+cur = bd.cursor()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS records (
+    name text,
+    score integer
+)"""
+            )
+def get_best_result():
+    cur.execute("""
+    SELECT name, MAX(score) AS score FROM records
+    GROUP BY name
+    ORDER BY score DESC
+    LIMIT 3
+    """
+                )
+    return cur.fetchall()
--- main.py
+++ main.py
@@ -1,6 +1,54 @@
 import pygame
 import sys
 from logics import *
+from database import get_best_result, cur
+
+
+def draw_intro():
+    img2048 = pygame.image.load("2048_logo.png")
+    font = pygame.font.SysFont("stxingkai", 70)
+    text_welcome = font.render("Welcome!", True, WHITE)
+    name = "Введите имя"
+    name_not_typed = True
+    while name_not_typed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                if event.unicode.isalpha():
+                    if name == "Введите имя":
+                        name = event.unicode
+                    else:
+                        name += event.unicode
+                elif event.key == pygame.K_BACKSPACE:
+                    name = name[:-1]
+                elif event.key == pygame.K_RETURN:
+                    if len(name) > 2:
+                        global username
+                        username = name
+                        name_not_typed = False
+        screen.fill(BLACK)
+        text_name = font.render(name, True, WHITE)
+        rect_name = text_name.get_rect()
+        rect_name.center = screen.get_rect().center
+        screen.blit(pygame.transform.scale(img2048, [200, 200]), [10, 10])
+        screen.blit(text_welcome, (230, 80))
+        screen.blit(text_name, rect_name)
+        pygame.display.update()
+    screen.fill(BLACK)
+
+
+def draw_top_gamers():
+    font_top = pygame.font.SysFont("simsun", 30)
+    font_gamer = pygame.font.SysFont("simsun", 20)
+    text_head = font_top.render("Best: ", True, COLOR_TEXT)
+    screen.blit(text_head, (250, 5))
+    for index, gamer in enumerate(GAMERS_DB):
+        name, score = gamer
+        s = f"{index + 1}. {name} - {score}"
+        text_gamer = font_gamer.render(s, True, COLOR_TEXT)
+        screen.blit(text_gamer, (250, 35 + 25 * index))
 
 
 def draw_interface(cur_score, cur_delta=0):
@@ -11,12 +59,13 @@ def draw_interface(cur_score, cur_delta=0):
     font_delta = pygame.font.SysFont("simsun", 32)
     text_score = font_score.render("Score: ", True, COLOR_TEXT)
     text_score_value = font_score.render(f" {cur_score}", True, COLOR_TEXT)
-    screen.blit(text_score, (80, 35))
-    screen.blit(text_score_value, (300, 35))
+    screen.blit(text_score, (20, 35))
+    screen.blit(text_score_value, (200, 35))
     if cur_delta > 0:
         text_score_delta = font_delta.render(f" +{cur_delta}", True, COLOR_TEXT)
-        screen.blit(text_score_delta, (290, 65))
+        screen.blit(text_score_delta, (190, 65))
     pretty_print(game_field)
+    draw_top_gamers()
     for row in range(blocks):
         for col in range(blocks):
             value = game_field[row][col]
@@ -31,6 +80,7 @@ def draw_interface(cur_score, cur_delta=0):
                 screen.blit(text, (text_x, text_y))
 
 
+username = ""
 game_field = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -39,14 +89,8 @@ game_field = [
 ]
 game_field[0][2] = 2
 game_field[2][1] = 4
-# n = 1
-# for i in range(4):
-#     for j in range(4):
-#         game_field[i][j] = 2**n
-#         n += 1
-#         n = min (n, 11)
-# game_field[-1][-1] = 0
 
+GAMERS_DB = get_best_result()
 COLORS = {
     0: (130, 130, 130),
     2: (255, 255, 255),
@@ -62,7 +106,7 @@ COLORS = {
     2048: (255, 150, 128),
 }
 COLOR_TEXT = (255, 127, 0)
-WHITE = (255, 255, 150)
+WHITE = (255, 255, 255)
 GREY = (130, 130, 130)
 BLACK = (0, 0, 0)
 score = 0
@@ -80,6 +124,7 @@ pretty_print(game_field)
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("2048")
+draw_intro()
 draw_interface(score)
 pygame.display.update()
 
